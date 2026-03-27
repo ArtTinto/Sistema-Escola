ad1049711bea5f05fa3b56de89db7a9c11ea3cf5
Artinto <arthuzao26@gmail.com>
2026-03-26 21:47:52 -0300

    Adiciona função de ler as disciplinas

--- Trabalho/src/Trabalho.java
+++ Trabalho/src/Trabalho.java
@@ -4,14 +4,14 @@ public class Trabalho {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int escolha = -1;
+        int escolha;
 
         Disciplina allDisiciplinas[] = new Disciplina[100];
 
         do {
-            int innerEscolha = 0;
+            int innerEscolha; // Inicia a variável a cada ciclo
 
-            System.out.println("Digite 0 para Sair");
+            System.out.println("\nDigite 0 para Sair");
             System.out.println("Digite 1 para Cadastrar");
             System.out.println("Digite 2 para Consultar");
             System.out.println("Digite 3 para Atualizar");
@@ -20,9 +20,9 @@ public class Trabalho {
 
             switch (escolha) {
                 case 1:
-                    // DoWhile feito para escolher qual categoria deseja adicionar ou voltar
+                    // Adicionar um aluno ou disciplinas
                     do {
-                        System.out.println("Digite 0 voltar");
+                        System.out.println("\nDigite 0 voltar");
                         System.out.println("Digite 1 para o cadastro de Disiciplinas");
                         System.out.println("Digite 2 para o cadastro de Alunos");
                         innerEscolha = scanner.nextInt();
@@ -31,32 +31,31 @@ public class Trabalho {
                     if (innerEscolha == 1) {
                         Disciplina dadosDisciplina = dadosDisciplina(scanner);
 
-                        addObjeto(allDisiciplinas, dadosDisciplina);
-                    } else if(innerEscolha == 2) {
+                        addDisciplina(allDisiciplinas, dadosDisciplina);
+                    } else if (innerEscolha == 2) {
 
                     }
 
                     break;
 
                 case 2:
-                    // DoWhile feito para escolher qual categoria deseja consultar ou voltar
+                    // Consultar os alunos ou Disciplinas
                     do {
-                        System.out.println("Digite 0 voltar");
-                        System.out.println("Digite 1 para o cadastro de Disiciplinas");
-                        System.out.println("Digite 2 para o cadastro de Alunos");
+                        System.out.println("\nDigite 0 voltar");
+                        System.out.println("Digite 1 para o consultar as Disiciplinas");
+                        System.out.println("Digite 2 para os alunos de Alunos");
                         innerEscolha = scanner.nextInt();
                     } while (innerEscolha > 2 || innerEscolha < 0);
                     if (innerEscolha == 1) {
-                        Disciplina dadosDisciplina = dadosDisciplina(scanner);
 
-                        addObjeto(allDisiciplinas, dadosDisciplina);
-                    } else if(innerEscolha == 2) {
+                    } else if (innerEscolha == 2) {
 
                     }
 
                     break;
 
                 case 3:
+                    //
                     break;
 
                 case 4:
@@ -67,7 +66,7 @@ public class Trabalho {
             }
 
         } while (escolha != 0);
-//Como o 
+        // Como o
     }
 
     static Disciplina dadosDisciplina(Scanner scanner) {
@@ -94,17 +93,38 @@ public class Trabalho {
         return cadDisiciplina;
     }
 
-    static boolean addObjeto(Disciplina[] arrObjeto, Disciplina objeto) {
-        if (arrObjeto != null) {
-            for (int i = 0; i < arrObjeto.length; i++) {
-                if (arrObjeto[i] == null) {
-                    arrObjeto[i] = objeto;
+    static boolean addDisciplina(Disciplina[] arrDisciplinas, Disciplina objeto) {
+        if (arrDisciplinas != null) {
+            for (int i = 0; i < arrDisciplinas.length; i++) {
+                if (arrDisciplinas[i] == null) {
+                    arrDisciplinas[i] = objeto;
 
                     return true;
                 }
             }
         }
-        return false;
 
+        return false;
+    }
+
+    static void printaDisiciplinas(Disciplina[] disciplinas) {
+        if (disciplinas != null) {
+            for (int i = 0; i < disciplinas.length; i++) {
+                if (disciplinas[i] != null) {
+
+                    System.out.println("Candidato: " + disciplinas[i].codigoDisiciplina);
+
+                    System.out.println("Nome: " + disciplinas[i].nomeDisciplina);
+
+                    System.out.println("Sigla: " + disciplinas[i].sigla);
+
+                    System.out.println("Ano disciplina: " + disciplinas[i].anoDisciplina);
+
+                    System.out.println("Nome do Professor: " + disciplinas[i].nomeProfessor);
+
+                    System.out.println();
+                }
+            }
+        }
     }
 }
