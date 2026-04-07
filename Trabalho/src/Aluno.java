28439125787e940b0103cd971a3249e6244b1e93
yuriespindolade-del <yuriespindolade@gmail.com>
2026-04-06 22:04:09 -0300

    Update Aluno.java

--- Trabalho/src/Aluno.java
+++ Trabalho/src/Aluno.java
@@ -2,7 +2,7 @@ public class Aluno {
     int CodigoAluno;
     String nomeAluno;
     String enderecoAluno;
-    String disciplinasMatriculadas;
-    String notasDisciplinas;
-    String notafinalDisciplina;
+    String[] disciplinasMatriculadas = new String[3]; // Exemplo de tamanho
+    int[] notasDisciplinas = new int[3]; 
+    double[] notasFinais = new double[3]; 
 }
