8c3a18a8a20e507453d9ee92dbf185763b7f50ff
yuriespindolade-del <yuriespindolade@gmail.com>
2026-05-21 12:53:38 -0300

    Update Aluno.java

--- Trabalho/src/Aluno.java
+++ Trabalho/src/Aluno.java
@@ -1,8 +1,46 @@
 public class Aluno {
     private static int qtdAlunos = 0;
-    private int CodigoAluno;
+    private int codigoAluno; 
     private String nomeAluno;
-    private double rg;
-    // Endereco endereco;
-    // Matricula[] matriculas = new Matricula[10];
+    private double rendimentoGlobal; 
+
+    public Aluno(String nomeAluno) {
+        if (nomeAluno != null && !nomeAluno.trim().isEmpty()) {
+            Aluno.qtdAlunos++;
+            this.codigoAluno = Aluno.qtdAlunos;
+            this.nomeAluno = nomeAluno;
+        } else {
+            throw new IllegalArgumentException("O nome do aluno não pode ser nulo ou vazio.");
+        }
+    }
+
+    public boolean setNomeAluno(String nomeAluno) {
+        if (nomeAluno != null && !nomeAluno.trim().isEmpty()) {
+            this.nomeAluno = nomeAluno;
+            return true;
+        }
+        return false;
+    }
+
+    public int getCodigoAluno() {
+        return this.codigoAluno;
+    }
+
+    public String getNomeAluno() {
+        return this.nomeAluno;
+    }
+
+    public double getRendimentoGlobal() {
+        return this.rendimentoGlobal;
+    }
+
+    
+    public void setRendimentoGlobal(double novaNota) {
+        if (novaNota >= 0.0 && novaNota <= 100.0) {
+            this.rendimentoGlobal = novaNota;
+        } else {
+            // Se a nota for menor que 0 ou maior que 10, o programa bloqueia a ação
+            throw new IllegalArgumentException("Nota inválida! O valor deve estar entre 0.0 e 100.0. Valor informado: " + novaNota);
+        }
+    }
 }
